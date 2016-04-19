f20879bf545891af3a2b2cea277f42f85cd7809f
codydegner <codyde9ner@gmail.com>
2016-04-19 16:10:02 -0500

    Added Other Regexes to search for

--- bot.js
+++ bot.js
@@ -5,7 +5,11 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
+      baileyRegex = /^\/bailey$/;
+      chaseRegex = /^\/chase$/;
+      spenceryRegex = /^\/spencer$/;
+      codyRegex = /^\/cody$/;
+      annaRegex = /^\/anna$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
